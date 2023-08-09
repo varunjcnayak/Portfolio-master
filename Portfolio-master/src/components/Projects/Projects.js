45f7b52f6d727781781588c6e75d0513752b3fa9
Varun Nayak K <varunjcnayak@gmail.com>
2023-08-09 20:41:03 +0530

    images changed

--- Portfolio-master/src/components/Projects/Projects.js
+++ Portfolio-master/src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import oracleERP from "../../Assets/Projects/oracle_erp_cloud.png";
+import bloodbank from "../../Assets/Projects/bloodbank.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import instaclone from "../../Assets/Projects/instaclone.jpg";
 
 function Projects() {
   return (
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={bloodbank}
               isBlog={false}
               title="Blood Bank Website"
               description="Real-Time Blood Bank Website with 2 factor authentication and notifier based on nearby radius."
@@ -34,7 +34,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={instaclone}
               isBlog={false}
               title="Instagram Clone"
               description="Instagram Clone Built using ReactJS and tailwind CSS and deployed on FireBase. Allows Like, Comment and Direct Message after signing up => signing in"
@@ -45,7 +45,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={oracleERP}
               isBlog={false}
               title="Oracle ERP"
               description="ERP solutions to client using Oracle VBCS, PL/SQL and Oracle SaaS"
