8cf678d03d5aa3e05952eb62478662fd7a36ad1f
Varun Nayak K <varunjcnayak@gmail.com>
2023-08-09 14:00:28 +0530

    changed Navbar href

--- Portfolio-master/src/components/Navbar.js
+++ Portfolio-master/src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href=""
                 target="_blank"
                 rel="noreferrer"
               >
