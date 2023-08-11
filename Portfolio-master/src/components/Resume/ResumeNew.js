8d16b7f1af99e77df025fffe66cd84a21a292ca4
Varun Nayak K <varunjcnayak@gmail.com>
2023-08-11 09:27:49 +0530

    resume google api to github link

--- Portfolio-master/src/components/Resume/ResumeNew.js
+++ Portfolio-master/src/components/Resume/ResumeNew.js
@@ -8,33 +8,19 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import axios from "axios";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const fileId = "1_FyGbv81koURCfVChApBkBpw_CuUzSRQ"; // Replace with your actual Google Drive file ID
-const apiKey = "AIzaSyCOQuG8oYhvXY6Q043W3yrMbfq-vQmOHDs"
-const googleDriveAPI = `https://www.googleapis.com/drive/v3/files/${fileId}`;
 const linkGit = "https://github.com/varunjcnayak/Portfolio-master/blob/master/Portfolio-master/src/Assets/VarunResumeAug.pdf";
 function ResumeNew() {
-  const [pdfLink, setPdfLink] = useState("");
+  
   const [pdfLinkGit,setPdfLinkGit] = useState("");
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
     setWidth(window.innerWidth);
     setPdfLinkGit(linkGit);
-    fetchPdfLink();
+    
   }, []);
 
-  const fetchPdfLink = async () => {
-    try {
-      const response = await axios.get(googleDriveAPI, {
-        responseType: "blob", // Fetch the file as a blob
-      });
-      const blob = new Blob([response.data], { type: "application/pdf" });
-      const pdfUrl = URL.createObjectURL(blob);
-      setPdfLink(pdfUrl);
-    } catch (error) {
-      console.error("Error fetching PDF link:", error);
-    }
-  };
+  
 
   return (
     <div>
